513bd01c7a88ce2b6501305b22bea793f5633603
heliosalesjr <65938536+heliosalesjr@users.noreply.github.com>
2025-04-14 18:21:04 -0300

    stand and run animation for both parts of the player character

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,10 +1,19 @@
 extends CharacterBody2D
+@onready var animation_player_lower: AnimationPlayer = $AnimationPlayerLower
+@onready var animation_player_upper: AnimationPlayer = $AnimationPlayerUpper
 
 func _physics_process(delta: float) -> void:
 	
 	var x_input = Input.get_axis("ui_left", "ui_right")
-
-	velocity.x = x_input * 50	
 	
+	velocity.x = x_input * 80	
+	
+	if x_input != 0:
+		animation_player_lower.play("run")
+		animation_player_upper.play("run")
+	else:
+		animation_player_lower.play("stand")
+		animation_player_upper.play("stand")
+		
 	move_and_slide()
 	
